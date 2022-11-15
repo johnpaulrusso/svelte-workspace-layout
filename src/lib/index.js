9b6bc320435ee00f0220d29df6065681a29541c0
johnpaulrusso <jprusso93@gmail.com>
2022-11-15 12:46:52 -0500

    Packaged library branch.
    Having trouble importing this package into other projects. Saving state for later.

---
+++ src/lib/index.js
@@ -0,0 +1,3 @@
+export {default as WorkspaceGrid} from './WorkspaceGrid.svelte'
+export {default as ContentWrapper} from './components/ContentWrapper.svelte'
+export {default as Sidebar} from './components/Sidebar.svelte'
